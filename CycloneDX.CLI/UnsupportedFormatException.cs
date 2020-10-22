2c899cf8689c672b98de7775f515aa3d841f67b5
Patrick Dwyer <coderpatros@outlook.com>
2020-10-22 22:38:12 +1000

    Add extra UnsupportedFormatException constructors

--- CycloneDX.CLI/UnsupportedFormatException.cs
+++ CycloneDX.CLI/UnsupportedFormatException.cs
@@ -4,6 +4,8 @@ namespace CycloneDX.CLI
 {
     public class UnsupportedFormatException : Exception
     {
+        public UnsupportedFormatException() : base() {}
         public UnsupportedFormatException(string message) : base(message) {}
+        public UnsupportedFormatException(string message, Exception innerException) : base(message, innerException) {}
     }
 }
